8a405b0a2af9d08e9839941a6681eeebb69338a6
Gabriel Mak <pixelbreaker@gmail.com>
2018-08-18 00:03:46 +0100

    homepage image tiles/animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,63 @@
 import React from 'react'
 // import { Link } from 'gatsby'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 
+const Tiles = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const Tile = styled.a`
+  flex: 0 0 50%;
+  padding-right: 10px;
+  text-decoration: none;
+  color: #999;
+`
+
+const TileImage = styled.div`
+  padding-bottom: 100%;
+  position: relative;
+  overflow: hidden;
+`
+
+const TileImg = styled.div`
+  background-size: cover;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+  right: 0;
+  top: 0;
+  transition: transform 0.5s ease;
+
+  ${Tile}:hover & {
+    transform: scale(1.1);
+  }
+`
+
+const TileLabel = styled.div`
+  color: #999;
+  font-size: 16px;
+  font-weight: 300;
+  margin: 10px 0;
+`
+
+
+
 const IndexPage = ({children, data}) => {
-  console.log(data)
   return (
     <Layout>
-      <h1>Hi people</h1>
-      {data.allContentfulPage.edges.map((page, index) => (
-        <>
-        <h2 key={index}>{page.node.title}</h2>
-        <img src={page.node.coverImage.file.url}/>
-        </>
-      ))}
+      <Tiles>
+        {data.allContentfulPage.edges.map((page, index) => (
+          <Tile href={page.node.slug} key={index}>
+            <TileImage>
+              <TileImg style={{ backgroundImage: `url(${page.node.coverImage.file.url})`}} />
+            </TileImage>
+            <TileLabel>{page.node.title}</TileLabel>
+          </Tile>
+        ))}
+      </Tiles>
     </Layout>
   )
 }
