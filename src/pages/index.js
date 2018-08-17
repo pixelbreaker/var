c8578af6a25594940285ed05100fc09aa2e3ea2d
Gabriel Mak <pixelbreaker@gmail.com>
2018-08-17 18:22:49 +0100

    Load content query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,42 @@
 import React from 'react'
-import { Link } from 'gatsby'
+// import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({children, data}) => {
+  console.log(data)
+  return (
+    <Layout>
+      <h1>Hi people</h1>
+      {data.allContentfulPage.edges.map((page, index) => (
+        <>
+        <h2 key={index}>{page.node.title}</h2>
+        <img src={page.node.coverImage.file.url}/>
+        </>
+      ))}
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allContentfulPage(filter:{ listed: { eq:true }}) {
+      edges {
+        node {
+          id,
+          slug,
+          title,
+          coverImage {
+            file {
+              url
+              fileName
+              contentType
+            }
+          }
+        }
+      }
+    }
+  }
+`
