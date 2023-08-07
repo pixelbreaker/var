f1ed7fa134ac5acae64bf716fb765c4481535b37
Gabes Mak <pixelbreaker@gmail.com>
2023-08-07 09:06:45 +0100

    Bodge GA

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
@@ -15,53 +15,75 @@ const PageRow = styled.div`
   padding: 0px 1.0875rem 1.45rem;
 `
 
-const Layout = ({ children, data }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+const Layout = ({ children, data }) => {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || []
+    function gtag() {
+      window.dataLayer.push(arguments)
+    }
+    gtag('js', new Date())
+
+    gtag('config', 'G-6PPQPT3J71')
+  }, [])
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+          contentfulHeaderLinks(title: { eq: "header" }) {
+            links {
+              title
+              slug
+            }
           }
         }
-        contentfulHeaderLinks(title: { eq: "header" }) {
-          links {
-            title
-            slug
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Var Studio' },
-            {
-              name: 'keywords',
-              content: 'design, branding, hong kong, london',
-            },
-          ]}
-        >
-          <html lang="en" />
-          <link
-            href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700&display=swap"
-            rel="stylesheet"
+      `}
+      render={data => (
+        <>
+          <Helmet
+            title={data.site.siteMetadata.title}
+            meta={[
+              { name: 'description', content: 'Var Studio' },
+              {
+                name: 'keywords',
+                content: 'design, branding, hong kong, london',
+              },
+            ]}
+          >
+            <html lang="en" />
+            <link
+              href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700&display=swap"
+              rel="stylesheet"
+            />
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-6PPQPT3J71"
+            ></script>
+            {/* <script
+            dangerouslySetInnerHTML={`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-6PPQPT3J71');`}
+          ></script> */}
+          </Helmet>
+          <Header
+            siteTitle={data.site.siteMetadata.title}
+            links={data.contentfulHeaderLinks.links}
           />
-        </Helmet>
-        <Header
-          siteTitle={data.site.siteMetadata.title}
-          links={data.contentfulHeaderLinks.links}
-        />
-        <PageRow>{children}</PageRow>
-        <PageRow>
-          <Footer />
-        </PageRow>
-      </>
-    )}
-  />
-)
+          <PageRow>{children}</PageRow>
+          <PageRow>
+            <Footer />
+          </PageRow>
+        </>
+      )}
+    />
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
